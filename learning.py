ea72fd6a847e52acb481117fc5627d90b37d3e7a
mehakjain10 <ms.mehakjain84@gmail.com>
2025-12-08 12:07:54 +0530

    added function to generalize the code

--- learning.py
+++ learning.py
@@ -1,5 +1,8 @@
-for num in range(10):
-    if num%2==0:
-        print(f'{num} is even')
-    else:
-        print(f'{num} is odd')
+def even_odd(start, end):
+    for num in range(start, end + 1):
+        if num%2==0:
+            print(f'{num} is even')
+        else:
+            print(f'{num} is odd')
+
+even_odd(100, 500)
